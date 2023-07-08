02800094675d4df7530b3f17521e925f0bf027c4
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-07-09 01:22:23 +0900

    画像step2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,11 +1,25 @@
 class PrototypesController < ApplicationController
-  def index 
+  def index
+    @prototype = Prototype.all
   end
 
   def new
-    @prototype = prototype.new
+    @prototype = Prototype.new
   end
 
-  def create 
+  def create
+    @prototype = Prototype.new(prototype_params)
+
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end

fbfc5bdc78ed1c3aaf2b139b924459af16a180f2
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-07-10 05:52:23 +0900

    first commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,63 @@
 class PrototypesController < ApplicationController
+<<<<<<< Updated upstream
   def index 
   end
 end
+=======
+
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  def index
+    @prototype = Prototype.all
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    prototype.update(prototype_params)
+    if prototype.save
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+end
+>>>>>>> Stashed changes
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root to: 'prototypes#index'
+<<<<<<< Updated upstream
   resources :prototypes, only: [:index]
+=======
+  resources :prototypes, only: [:index,:show,:new,:create,:edit,:update,:destroy]do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
+>>>>>>> Stashed changes
 end
